161d7c00720ba97e1cda383b60207aa4b49d0705
princz19 <princeji4562@gmail.com>
2024-02-21 12:00:37 +0530

    change

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("prince mishra")
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Server started...!')
+})
+
+app.get('/instegram',(req,res)=>{
+     res.send("@beingsprince")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
